533aaed014db2cd3b0755837a6abdbd6b8c2e53d
Eric Rovtar <eric.rovtar@me.com>
2017-02-08 22:02:30 -0500

    Clean up spacing

--- vuex-test/src/store/actions.js
+++ vuex-test/src/store/actions.js
@@ -3,15 +3,14 @@ import * as api from '../api';
 export const loadData = ({ commit }) =>  {
     api.loadData()
         .then(function json(response) {  
-                return response.json()  
-            })
+            return response.json()  
+        })
         .then(function(data) {
-                // _this.setState({ ministryPartners: data });
-                console.log('Request succeeded with JSON response', data);
-                commit('loadData', { data });
-            })
+            // _this.setState({ ministryPartners: data });
+            console.log('Request succeeded with JSON response', data);
+            commit('loadData', { data });
+        })
         .catch(function(error) {
-                console.log('Request failed', error);
-            });
-
+            console.log('Request failed', error);
+        });
 };
